346380ecc5abea3871464f54deb4eee81c202feb
root <franklolxu@gmail.com>
2021-08-26 21:46:29 +0000

    The project is finished

---
+++ sql_queries.py
@@ -0,0 +1,108 @@
+# DROP TABLES
+
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
+
+# CREATE TABLES
+
+songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays 
+(songplay_id SERIAL PRIMARY KEY,
+start_time TIMESTAMP,
+user_id varchar,
+level varchar,
+song_id varchar,
+artist_id varchar,
+session_id varchar,
+localtion varchar,
+user_agent varchar)
+""")
+
+user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users 
+(user_id varchar PRIMARY KEY,
+first_name varchar,
+last_name varchar,
+gender varchar,
+level varchar)
+""")
+
+song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs 
+(song_id varchar PRIMARY KEY,
+title varchar,
+artist_id varchar,
+year int,
+duration float)
+""")
+
+artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists 
+(artist_id varchar PRIMARY KEY,
+name varchar,
+location varchar,
+latitude float,
+longitude float)
+""")
+
+time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time 
+(start_time timestamp,
+year int,
+month int,
+week int,
+day int,
+weekday int,
+hour int)
+""")
+
+# INSERT RECORDS
+
+songplay_table_insert = ("""
+INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, localtion, user_agent)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+""")
+
+user_table_insert = ("""
+INSERT INTO users (user_id, first_name, last_name, gender, level)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (user_id)
+DO UPDATE SET first_name = EXCLUDED.first_name, last_name = EXCLUDED.last_name, 
+              gender = EXCLUDED.gender, level = EXCLUDED.level
+""")
+
+song_table_insert = ("""
+INSERT INTO songs (song_id, title, artist_id, year, duration)
+VALUES (%s, %s, %s, %s, %s)
+""")
+
+artist_table_insert = ("""
+INSERT INTO artists (artist_id, name, location, latitude, longitude)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (artist_id)
+DO UPDATE SET name = EXCLUDED.name, location = EXCLUDED.location, 
+              latitude = EXCLUDED.latitude, longitude = EXCLUDED.longitude
+""")
+
+
+time_table_insert = ("""
+INSERT INTO time (start_time, year, month, week, day, weekday, hour)
+VALUES (%s, %s, %s, %s, %s, %s, %s)
+""")
+
+# FIND SONGS
+# Implement the song_select query in sql_queries.py to find the song ID and artist ID based on the title, artist name, and duration of a song.
+
+song_select = ("""
+SELECT a.song_id, a.artist_id FROM songs a
+JOIN artists b ON a.artist_id = b.artist_id
+WHERE a.title = ''%s'' AND b.name = ''%s'' AND a.duration = %s
+""")
+
+# QUERY LISTS
+
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
